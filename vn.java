0aff0b91b1cc8ef224813f98c48d32ea3082ef7c
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 15:36:40 +0530

    Create vn.java

---
+++ vn.java
@@ -0,0 +1,34 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		System.out.println("Enter value");
+		Scanner sc=new Scanner(System.in);
+		
+	
+		
+	try{
+		int nn=sc.nextInt();
+		System.out.println("this is numeric");
+	}catch(Exception e){
+		System.err.println("this is character");
+	}
+	
+		
+	
+			
+			
+			
+		
+
+	}
+
+}

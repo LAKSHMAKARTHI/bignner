3875d9ffde5a04a5cfa418e4af415a1b495127b0
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 16:03:10 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,44 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		
+		int n1,n2,n3;
+		System.out.println("Enter the numbers:");
+		Scanner s=new Scanner(System.in);
+		
+		int n=s.nextInt();
+		
+		n=n%4;
+		
+		if(n==0){
+			System.out.println("leap year");
+		}else
+		{
+			System.out.println("year not leap year");
+		}
+		
+		
+		
+
+	
+	}	
+	
+	}
+	
+			
+			
+			
+		
+
+	
+
+

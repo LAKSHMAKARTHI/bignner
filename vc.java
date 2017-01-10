7f77e2db6270a698f8c38e7c246a907075122280
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 15:28:43 +0530

    Create vc.java

---
+++ vc.java
@@ -0,0 +1,35 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		System.out.println("Enter value");
+		Scanner sc=new Scanner(System.in);
+		
+	String nn=sc.next();
+		
+	char n=nn.charAt(0);
+		
+		if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'){
+			
+			System.out.println("this is vowel character");
+		}else
+		{
+			System.out.println("this is consonant number");
+		}	
+	
+			
+			
+			
+		
+
+	}
+
+}

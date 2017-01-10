6496c45345a4365100a17972dde93951bf5a81be
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 15:16:59 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,35 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		System.out.println("Enter value");
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		n=n%2;
+		if(n==0){
+			
+			System.out.println("this is even number");
+		}else
+		{
+			System.out.println("this is odd number");
+		}	
+	
+			
+			
+			
+		
+		
+
+	}
+
+}

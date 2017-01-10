df9cf56d52419c38599e998951b6fdce51c318ae
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 16:07:20 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,42 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		
+		int n1,n2,n3;
+		System.out.println("Enter the numbers:");
+		Scanner s=new Scanner(System.in);
+		
+		int n=s.nextInt();
+		
+		
+		int i=0;
+		while(i<n){
+			System.out.println("Hello");
+			i++;
+		}
+		
+		
+		
+
+	
+	}	
+	
+	}
+	
+			
+			
+			
+		
+
+	
+
+

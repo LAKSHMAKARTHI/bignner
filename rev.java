2e4cc8fde4c6c48496735ef1ecd2e5d9dc15e688
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 15:57:16 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,41 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		
+		int n1,n2,n3;
+		System.out.println("Enter the numbers:");
+		Scanner s=new Scanner(System.in);
+		n1=s.nextInt();
+		n2=s.nextInt();
+		n3=s.nextInt();
+		if(n1>n2&&n1>n3)
+		System.out.println(+n1+"is largest");
+		else if(n2>n3)
+			System.out.println(+n2+"is largest");
+		else
+			System.out.println(+n3+"is largest");
+		}
+		
+		
+		
+
+	
+		
+	
+			
+			
+			
+		
+
+	}
+
+

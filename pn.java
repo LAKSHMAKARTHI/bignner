a80b49fe675cc62a5b2bc93e26327388cbf882a1
Lakshmakarthi.A <a.lakshmakarthi@gmail.com>
2017-01-10 15:12:43 +0530

    Create pn.java

---
+++ pn.java
@@ -0,0 +1,37 @@
+package com.cogzidel.lk;
+
+
+import java.util.*;
+
+public class Sample10 {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+		System.out.println("Enter value");
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		if(n==0){
+			
+			System.out.println("this is 0 value");
+		}else if(n<0){
+			System.out.println("this is nagative");
+		}else
+		{
+			System.out.println("this is pasitive");
+		}
+		
+	
+			
+			
+			
+		
+		
+
+	}
+
+}
